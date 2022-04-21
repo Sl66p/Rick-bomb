7d506becbf3894a49ae6deaca556a63e3b8de486
Sl66p <60981795+Sl66p@users.noreply.github.com>
2022-04-21 17:54:37 -0400

    Working Version V1!!

--- RickBomb.py
+++ RickBomb.py
@@ -1,10 +1,14 @@
 from re import A
 from ssl import AlertDescription
 from tkinter.messagebox import YES, showinfo
+from turtle import goto
+from tkinter import *
+from tkinter import messagebox
 import webbrowser
 import ctypes  # An included library with Python install, draws boxes, makes alert screens
 
-# RollLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+# Rickroll Link :)
+RollLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
 # Draw an Alert Box, with options
 ##  Styles:
@@ -21,6 +25,20 @@ import ctypes  # An included library with Python install, draws boxes, makes ale
   # return ctypes.windll.user32.MessageBoxW(0, text, title, style)
 # Mbox('Your title', 'Your text', 1)
 
-def Mbox(title, text, style):
-    return ctypes.windll.user32.MessageBoxW(0, text, title, 0)
-Mbox('Get Rolled!', 'Prepare to be Rolled!', 0)
+#def Mbox(title, text, style):
+#    return ctypes.windll.user32.MessageBoxW(0, text, title, 0)
+#Mbox('Get Rolled!', 'Prepare to be Rolled!', 0)
+
+#if Mbox == 0:
+     # goto loop
+
+
+
+
+loop = 1
+
+while loop > 0:
+      webbrowser.open_new(RollLink)
+      messagebox.askretrycancel("askretrycancel", "Get Rolled!")
+      
+      loop += 1
