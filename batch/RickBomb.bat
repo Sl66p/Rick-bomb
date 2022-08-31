9af48b5262a7c7b8f826e62641dcd66a4edd7ed6
Collin Laney <95191733+ColDog5044@users.noreply.github.com>
2022-08-31 09:01:11 -0400

    Effieciencycyyy

---
+++ batch/RickBomb.bat
@@ -0,0 +1,41 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBdVQgeNMDvvVuAd1Mz07P+Kp0MJR94YcZvzz7ayEukQ4QjGepsv02hmqsIPBzlXcxDlaxcxyQ==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+
+:: Script by ColDog Studios (ColDog5044)
+:: Last Modified: 8/31/2022
+@echo off
+:loop
+start chrome https://www.youtube.com/watch?v=dQw4w9WgXcQ
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('You have been Rick Rolled by Sl66p and ColDog Studios', 'Rick Bomber', 'Ok', [System.Windows.Forms.MessageBoxIcon]::Exclamation);}"
+goto loop
